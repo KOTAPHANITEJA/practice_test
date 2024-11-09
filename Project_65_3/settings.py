781ba1c266b7c107b2b10ce12e95302f2a6eba41
KOTAPHANITEJA <phanitejakota@gmail.com>
2024-11-09 08:53:09 +0530

    Added requirements.txt

--- Project_65_3/settings.py
+++ Project_65_3/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-wh+*7n5=-3y!o(p%t(mro6up63c^%#1)7e@oly(_tiyn=#orx6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['kotaphaniteja.onrender.com','127.0.0.1']
 
 # Application definition
 
